c90399a54f8d57c58b92203bb7f7acea66c042c0
MalkiLevinzon <m0534191702@gmail.com>
2023-10-23 14:58:29 +0300

    t

--- MyFridge/MyFridge/Program.cs
+++ MyFridge/MyFridge/Program.cs
@@ -114,26 +114,25 @@
             shelf.Add(new Shelf(4, items));
             Refrigerator refrigerator = new("amkor", "blake", shelf);
             List<Item> items2 = new List<Item>();
-            items.Add(new Item("milk", KindItem.Drink, Kosher.Dairy, DateTime.Now, 78));
-            items.Add(new Item("pizza", KindItem.Food, Kosher.Dairy, DateTime.Now.AddDays(11), 11));
-            items.Add(new Item("woter", KindItem.Drink, Kosher.Fur, DateTime.Now.AddDays(80), 4));
+            items2.Add(new Item("milk", KindItem.Drink, Kosher.Dairy, DateTime.Now, 78));
+            items2.Add(new Item("pizza", KindItem.Food, Kosher.Dairy, DateTime.Now.AddDays(11), 11));
+            items2.Add(new Item("woter", KindItem.Drink, Kosher.Fur, DateTime.Now.AddDays(80), 4));
             List<Shelf> shelf2 = new List<Shelf>();
-            shelf.Add(new Shelf(1, items2));
-            shelf.Add(new Shelf(2, items));
-            shelf.Add(new Shelf(3, items));
-            shelf.Add(new Shelf(4, items2));
+            shelf2.Add(new Shelf(1, items2));
+            shelf2.Add(new Shelf(2, items));
+            shelf2.Add(new Shelf(3, items));
+            shelf2.Add(new Shelf(4, items2));
             Refrigerator refrigerator2 = new("sumsong", "silver", shelf2);
             List<Refrigerator> refrigerators = new();
             refrigerators.Add(refrigerator2);
             refrigerators.Add(refrigerator);
 
-
-            Console.WriteLine("Press 1: the program will print all the items on the refrigerator and all its contents.\r\nClick 2: the program will print how much space is left in the fridge\r\nPress 3: The program will allow the user to put an item in the fridge.\r\nPress 4: The program will allow the user to remove an item from the refrigerator.\r\nPress 5: the program will clean the refrigerator and print all the checked items to the user.\r\nPress 6: the program will ask the user &quot;What do I want to eat?&quot; and bring the function to bring a product.\r\nClick 7: the program will print all the products sorted by their expiration date.\r\nPress 8: the program will print all the shelves arranged according to the free space left on them.\r\nPress 9: the program will print all the refrigerators arranged according to the free space left in them.\r\nClick 10: The program will prepare the refrigerator for shopping\r\nPress 100: system shutdown.");
+            Writline();
             string input = "0";
             while (input != "100")
             {
                 Console.WriteLine("press a number from 1-10");
-                input = Console.ReadLine()??"not null";
+                input = Console.ReadLine() ?? "not null";
                 switch (input)
                 {
                     case "1":
@@ -146,20 +145,20 @@
                         HoutItem(refrigerator); break;
                     case "5":
                         Cleaning(refrigerator); break;
-                        case "6":
+                    case "6":
                         WantToEat(refrigerator); break;
                     case "7":
-                        PrintEExpiryDate(refrigerator);break;
+                        PrintEExpiryDate(refrigerator); break;
                     case "8":
-                        PrintByAvailabilityShelfe(refrigerator);break;
-                        case "9":
-                        PrintByAvailabilityFrige(refrigerators);break;
-                        case "10":
-                        refrigerator.GoingShopping();break;
+                        PrintByAvailabilityShelfe(refrigerator); break;
+                    case "9":
+                        PrintByAvailabilityFrige(refrigerators); break;
+                    case "10":
+                        refrigerator.GoingShopping(); break;
                     case "100":
-                        Console.WriteLine("good by...");break;
+                        Console.WriteLine("good by..."); break;
                     default:
-                        Console.WriteLine("Invalid input");break;
+                        Console.WriteLine("Invalid input"); break;
 
 
 
@@ -171,6 +170,10 @@
             }
         }
 
-       
+        private static void Writline()
+        {
+            Console.WriteLine("Press 1: the program will print all the items on the refrigerator and all its contents.\r\nClick 2: the program will print how much space is left in the fridge\r\nPress 3: The program will allow the user to put an item in the fridge.\r\nPress 4: The program will allow the user to remove an item from the refrigerator.\r\nPress 5: the program will clean the refrigerator and print all the checked items to the user.\r\nPress 6: the program will ask the user &quot;What do I want to eat?&quot; and bring the function to bring a product.\r\nClick 7: the program will print all the products sorted by their expiration date.\r\nPress 8: the program will print all the shelves arranged according to the free space left on them.\r\nPress 9: the program will print all the refrigerators arranged according to the free space left in them.\r\nClick 10: The program will prepare the refrigerator for shopping\r\nPress 100: system shutdown.");
+        }
+
     }
 }
